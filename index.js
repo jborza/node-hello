8c10ebae90a15fe18c3dcdbebd151486d8e42259
Juraj Borza <jborza@gmail.com>
2020-01-15 13:16:49 +0100

    added time display

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello World!'); 
+  res.write('Hello World! The time is: '+new Date().toLocaleTimeString('en-US')); 
   res.end(); 
 }).listen(8080);  
